107ce0397d26cae2ddc0235afbc074db6333557e
Min Thu Kyaw Khaung <204016955+markus1525@users.noreply.github.com>
2025-11-23 16:47:37 +0800

    Update assembly information and remove outdated README file

--- obj/Debug/net9.0/ShapeDrawer.AssemblyInfo.cs
+++ obj/Debug/net9.0/ShapeDrawer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e522b96ac56a6167a46beade213f08d259fd886b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b9b5afb7af59b6c06f0a4e592d3c3368d2274f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
